a1ab0a0e9f3ab3a6cd512f4bf82b44ea4f557d86
Mohamed-Hiring <m.khalid.skd@gmail.com>
2025-04-02 09:45:05 +0300

    Update cardContent.tsx

--- components/ui/cardContent.tsx
+++ components/ui/cardContent.tsx
@@ -1,1 +1,10 @@
-export default function CardContent({ children, ...props }) { return <div {...props}>{children}</div>; }
+import { cn } from "../../lib/utils";
+import { HTMLAttributes } from "react";
+
+interface CardProps extends HTMLAttributes<HTMLDivElement> {}
+
+export default function CardContent({ className, ...props }: CardProps) {
+  return (
+    <div className={cn("p-4", className)} {...props} />
+  );
+}
