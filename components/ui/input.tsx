a390ceac4075fa66557299ff93af33c89d3f532a
Mohamed-Hiring <m.khalid.skd@gmail.com>
2025-04-02 09:44:31 +0300

    Update input.tsx

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -1,1 +1,14 @@
-export default function Input(props) { return <input {...props} />; }
+import React from "react";
+
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
+
+const Input: React.FC<InputProps> = ({ className = "", ...props }) => {
+  return (
+    <input
+      {...props}
+      className={`border border-gray-300 rounded-md px-3 py-2 w-full focus:outline-none focus:ring-2 focus:ring-purple-500 ${className}`}
+    />
+  );
+};
+
+export default Input;
