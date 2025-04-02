6089d3cb71bcf26f491bbe1c4519164af5a668f7
Mohamed-Hiring <m.khalid.skd@gmail.com>
2025-04-02 09:44:42 +0300

    Update label.tsx

--- components/ui/label.tsx
+++ components/ui/label.tsx
@@ -1,1 +1,14 @@
-export default function Label(props) { return <label {...props} />; }
+import React from "react";
+
+interface LabelProps extends React.LabelHTMLAttributes<HTMLLabelElement> {}
+
+const Label: React.FC<LabelProps> = ({ className = "", ...props }) => {
+  return (
+    <label
+      {...props}
+      className={`block text-sm font-medium text-gray-700 ${className}`}
+    />
+  );
+};
+
+export default Label;
